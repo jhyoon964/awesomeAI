888a0815ea1a1d89dd15c07dcbb5b08b7c45308f
jhyoon964 <144157648+jhyoon964@users.noreply.github.com>
2025-12-21 17:53:42 +0900

    fix: test file

--- tools/test.sh
+++ tools/test.sh
@@ -61,7 +61,8 @@ main() {
     -d "$SITE_DIR$_baseurl" -c "$_config"
 
   # test
-  bundle exec htmlproofer "$SITE_DIR" \
+  # bundle exec htmlproofer "$SITE_DIR" \
+  bundle exec htmlproofer "$SITE_DIR$_baseurl" \
     --disable-external \
     --ignore-urls "/^http:\/\/127.0.0.1/,/^http:\/\/0.0.0.0/,/^http:\/\/localhost/"
 }
